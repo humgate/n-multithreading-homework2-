b7964c3551272807823d650f8402d61a5d9ab0b2
Pavel Yanovsky <humgate@gmail.com>
2021-12-07 22:55:16 +0300

    Добавлено покупка 10 машин покупателями. Добавлено описание. Небольшие правки javadoc

--- car-retailer/src/main/java/Main.java
+++ car-retailer/src/main/java/Main.java
@@ -2,7 +2,14 @@
  * Запускает вселенную
  */
 public class Main {
-     public static void main(String[] args) {
+    //таймаут желания купить машину покупателем
+    static final int BYER_TIMEOUT = 8000;
+
+    public static void main(String[] args) {
+        /*
+         * Создаем сущности и существ
+         */
+
         //да будет салон
         final CarRetailer dealer = new CarRetailer();
 
@@ -15,15 +22,35 @@ public class Main {
         //а третий менеджер будет только работать с заводом по приемке машин
         final Manager manThree = new Manager(dealer, "Петя-приемщик");
 
-        //Запускаем нашу вселенную
-        //производитель
+        /*
+         *Запускаем потоки нашей вселенной
+         */
+
+        //производитель совмещенный с приемщиком начинает произодвить-принимать машины
         new Thread(null, manThree::receiveCar, "Производитель авто").start();
 
-        //покупатель подошедший к первому менеджеру
-        new Thread(null, manOne::sellCar, "Покупатель 1").start();
+        //приходят покупатели которые в сумме собираются купить 10 машин
 
-        //покупатель подошедший к второму менеджеру
-        new Thread(null, manTwo::sellCar, "Покупатель 2").start();
+        //подошедший к первому менеджеру по продажам
+        new Thread(null, manOne::sellCar, "Покупатель 1. маш1").start();
+
+        //эти 3 покупателя планируют купить каждый по 3 машины
+        for (int i = 0; i < 3; i++) {
+            //подошедшие к первому менеджеру по продажам
+            new Thread(null, manOne::sellCar, "Покупатель 2. маш" + (i + 1)).start();
+
+            //подошедшие к второму менеджеру по продажам
+            new Thread(null, manTwo::sellCar, "Покупатель 3. маш" + (i + 1)).start();
+
+            //подошедшие к второму менеджеру по продажам
+            new Thread(null, manTwo::sellCar, "Покупатель 4. маш" + (i + 1)).start();
+
+            try {
+                Thread.sleep(BYER_TIMEOUT);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
